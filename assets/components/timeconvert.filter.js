4c818cc7648cdc64aa1c164abaa51e726c860d69
TK <tobiaskoenig1@googlemail.com>
2016-11-12 16:01:14 +0100

    using filter for converting filename to date or time

---
+++ assets/components/timeconvert.filter.js
@@ -0,0 +1,25 @@
+angular.module("cv.components")
+    .filter("timeConvert", timeConvertFilter);
+
+function timeConvertFilter() {
+    return function (value, mode) {
+        var matchResult;
+        if (value) {
+            if (mode === "time") {
+                if ((matchResult = value.match(/[0-9]{6}/))) {
+                    value = matchResult[0];
+                    value = value.slice(0, 2) + ":" + value.slice(2, 4) + " Uhr";
+                } else if ((matchResult = value.match(/[0-9]{2}_[0-9]{2}_[0-9]{2}/))) {
+                    value = matchResult[0];
+                    value = value.slice(0, 2) + ":" + value.slice(3, 5) + " Uhr";
+                }
+            } else if (mode === "date") {
+                if ((matchResult = value.match(/[0-9]{8}/))) {
+                    value = matchResult[0];
+                    value = value.slice(6, 8) + "." + value.slice(4, 6) + "." + value.slice(0, 4);
+                }
+            }
+        }
+        return value;
+    };
+}
